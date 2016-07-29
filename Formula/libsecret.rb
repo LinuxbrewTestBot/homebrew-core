7f24708a9990801f878194f67c268a4e7a928bf8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-07-29 18:11:54 +0000

    libsecret: update 0.18.5 bottle for Linuxbrew.

--- Formula/libsecret.rb
+++ Formula/libsecret.rb
@@ -5,9 +5,7 @@ class Libsecret < Formula
   sha256 "9ce7bd8dd5831f2786c935d82638ac428fa085057cc6780aba0e39375887ccb3"
 
   bottle do
-    sha256 "07d7fd02e1c3857a2a27415575248ea89a19c7dd1a86efe53fcf1866dce1bf46" => :el_capitan
-    sha256 "090ea52539396135f710956cd051cbab59d6dd7fb94d8666dc4ed4e0312cddac" => :yosemite
-    sha256 "b7f878856f2f272de7d0456ad3c82a6e260a9a272a7c4eefc1504a668a430fb0" => :mavericks
+    sha256 "9a875f5a0ad4d45f01431d73da21eb2e8087023114526349a1d122ef28a25451" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
