48d650c0552a645356f1e59f777dd639d4401013
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-08-01 20:57:16 +0000

    libxml++3: update 3.0.0 bottle for Linuxbrew.

--- Formula/libxml++3.rb
+++ Formula/libxml++3.rb
@@ -6,9 +6,7 @@ class Libxmlxx3 < Formula
 
   bottle do
     cellar :any
-    sha256 "1eee12f428fea153679ce88eaf7f334f00ceb64aea6cf139346b535c5a664554" => :el_capitan
-    sha256 "e068fff70a7513e0225288bc6092f37a016b4269d3a76c3997be1ba5a964c4e9" => :yosemite
-    sha256 "8987284c0005cd2b8b80d91b81f13783e4e7d1ebff1f794b4686891381614b1a" => :mavericks
+    sha256 "a61a7cd171acee56337bb905172b859f952c00b900532f9059b5febdaae9cb0d" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
