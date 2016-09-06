c2473cc99cbbacaf70bf109c2b6d95bc65b32f2c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-09-06 00:53:26 +0000

    recutils: update 1.7 bottle for Linuxbrew.

--- Formula/recutils.rb
+++ Formula/recutils.rb
@@ -7,11 +7,12 @@ class Recutils < Formula
 
   bottle do
     cellar :any
-    revision 1
+    rebuild 1
     sha256 "b05bab96785a80e5bef03d810ab0f764db53856e1fba5fa65d2b4db25fe3575c" => :el_capitan
     sha256 "b91cd49a977ff93d079ac65a637b6d9681f045368e06a0a1d630ca97e14bd11a" => :yosemite
     sha256 "7e323f2500199e0b1d8fbbd26b33c022c56a9582abdefa401c4629d7152b7f4d" => :mavericks
     sha256 "aec2464c5e26a561b340e9ae5a080366a068936ff2ba4e86e6a4bcf0ed8a95d0" => :mountain_lion
+    sha256 "e2e078f38fc397c58198f40e8a8a514c36a0d94430f67c9142442e1f37612f12" => :x86_64_linux
   end
 
   depends_on "libgcrypt" unless OS.mac?
