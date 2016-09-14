d9fd9f89d30465e98fb3c20db0cb239f4a59210a
Bob W. Hogg <rwhogg@linux.com>
2016-09-14 10:31:12 -0400

    git: Fix test for Linuxbrew
    Commiting can fail if you lack proper git configuration for username
    and email, so add (dummy) ones in before commiting.

--- Formula/git.rb
+++ Formula/git.rb
@@ -174,8 +174,15 @@ class Git < Formula
   end
 
   test do
+    ohai "#{system "whoami"}"
+
     system bin/"git", "init"
     %w[haunted house].each { |f| touch testpath/f }
+
+    # Test environment has no git configuration, which prevents commiting
+    system bin/"git", "config", "user.email", "you@example.com"
+    system bin/"git", "config", "user.name", "Your Name"
+
     system bin/"git", "add", "haunted", "house"
     system bin/"git", "commit", "-a", "-m", "Initial Commit"
     assert_equal "haunted\nhouse", shell_output("#{bin}/git ls-files").strip
