1f9e25fba273d76ad1ecca2af2937b6268d3c4bf
Don <hmbright@fastmail.com>
2016-10-16 18:52:49 +0000

    qt5: fix missing :x11 dependency , copy from qt.rb

--- Formula/qt5.rb
+++ Formula/qt5.rb
@@ -46,6 +46,8 @@ class Qt5 < Formula
   depends_on :postgresql => :optional
   depends_on :xcode => :build
 
+  depends_on :x11 unless OS.mac?
+
   depends_on OracleHomeVarRequirement if build.with? "oci"
 
   resource "qt-webkit" do
