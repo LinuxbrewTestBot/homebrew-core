b4d6088d7e46e92121bbf995082ed51aaeb794d5
Maxim Belkin <maxim.belkin@gmail.com>
2016-11-29 15:29:39 -0600

    libprelude: final fixes

--- Formula/libprelude.rb
+++ Formula/libprelude.rb
@@ -4,38 +4,44 @@ class Libprelude < Formula
   url "https://www.prelude-siem.org/attachments/download/721/libprelude-3.1.0.tar.gz"
   sha256 "b8fbaaa1f2536bd54a7f69fe905ac84d936435962c8fc9de67b2f2b375c7ac96"
 
+  option "without-ruby", "Build without Ruby bindings"
+
   depends_on "pkg-config" => :build
-  depends_on "glibc"
-  depends_on "gmp"
-  depends_on "gnutls"
   depends_on "libgcrypt"
-  depends_on "libgpg-error" # optional / recommended ?
-  depends_on "libtasn1" # optional / recommended ?
-  depends_on "libidn" # optional / recommended ?
-  depends_on "nettle" # optional / recommended ?
-  depends_on "libtool"
-  depends_on "swig" => :recommended
-  depends_on "bison" => :recommended
-  depends_on "flex" => :recommended
-  depends_on "perl" => :optional
-  depends_on :python => :optional
-  depends_on "python3" => :optional
+  depends_on "gnutls"
+  depends_on "swig" => [:build, :recommended]
+  depends_on "perl" => [:build, :optional]
+  depends_on :python => [:build, :recommended]
+  depends_on :python3 => [:build, :recommended]
+  depends_on "valgrind" => [:build, :recommended]
+  depends_on "lua" => [:build, :optional]
 
   skip_clean "etc", "lib64", "var", :la
 
   def install
-    ENV["CXX"] = "gcc" # CXX has to be set to "gcc"
-    system "./configure"
-      "--disable-dependency-tracking",
-      "--prefix=#{prefix}",
-      "--without-lua"
+    ENV["HAVE_CXX"] = "yes"
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --disable-rpath
+      --with-pic
+    ]
 
+    args << "--with-libgcrypt-prefix=#{Formula["libgcrypt"].opt_prefix}" if build.with? "libgcrypt"
+    args << "--with-libgnutls-prefix=#{Formula["gnutls"].opt_prefix}" if build.with? "gnutls"
+
+    ["swig", "perl", "python2", "python3", "valgrind", "lua", "ruby"].each do |r|
+      args << "--with-#{r}=#{build.with?(r) ? "yes" : "no"}"
+    end
+
+    system "./configure", *args
     system "make"
     system "make", "check"
     system "make", "install"
   end
 
   test do
-    #
+    assert_equal prefix.to_s, shell_output(bin/"libprelude-config --prefix").chomp
+    assert_equal version.to_s, shell_output(bin/"libprelude-config --version").chomp
   end
 end
