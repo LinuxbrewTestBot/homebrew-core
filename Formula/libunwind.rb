94ac7aabb360b8fd8805b84d9d9f94f9d269d5ba
Maxim Belkin <maxim.belkin@gmail.com>
2017-03-21 17:22:33 -0500

    libunwind: update 1.12 bottle for Linuxbrew

--- Formula/libunwind.rb
+++ Formula/libunwind.rb
@@ -6,10 +6,6 @@ class Libunwind < Formula
   head "git://git.sv.gnu.org/libunwind.git"
   # tag "linuxbrew"
 
-  bottle do
-    sha256 "caabeb18f79a24a02caf5c3f706c144173310410bddd470160909b3eda391a41" => :x86_64_linux
-  end
-
   depends_on "xz"
 
   def install
