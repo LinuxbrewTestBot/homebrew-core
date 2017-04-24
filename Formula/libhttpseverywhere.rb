b94eeb18ef2cfed13ca9b1c80d12af6ac100887f
Maxim Belkin <maxim.belkin@gmail.com>
2017-04-24 02:14:47 -0500

    libhttpseverywhere: fixes for Linuxbrew

--- Formula/libhttpseverywhere.rb
+++ Formula/libhttpseverywhere.rb
@@ -20,6 +20,7 @@ class Libhttpseverywhere < Formula
   depends_on "libsoup"
   depends_on "libgee"
   depends_on "libarchive"
+  depends_on "libxml2" unless OS.mac?
 
   def install
     mkdir "build" do
@@ -47,6 +48,7 @@ class Libhttpseverywhere < Formula
     libgee = Formula["libgee"]
     libsoup = Formula["libsoup"]
     pcre = Formula["pcre"]
+    libxml2 = Formula["libxml2"]
     flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
     flags += %W[
       -I#{gettext.opt_include}
@@ -58,6 +60,9 @@ class Libhttpseverywhere < Formula
       -I#{libgee.opt_include}/gee-0.8
       -I#{libsoup.opt_include}/libsoup-2.4
       -I#{pcre.opt_include}
+    ]
+    flags << "-I#{libxml2.opt_include}/libxml2" unless OS.mac?
+    flags += %W[
       -D_REENTRANT
       -L#{gettext.opt_lib}
       -L#{glib.opt_lib}
@@ -65,18 +70,18 @@ class Libhttpseverywhere < Formula
       -L#{libarchive.opt_lib}
       -L#{libgee.opt_lib}
       -L#{libsoup.opt_lib}
-      -L#{lib}
+      -L#{OS.mac? ? lib : lib/"x86_64-linux-gnu"}
       -larchive
       -lgee-0.8
       -lgio-2.0
       -lglib-2.0
       -lgobject-2.0
       -lhttpseverywhere-0.4
-      -lintl
       -ljson-glib-1.0
       -lsoup-2.4
       -lxml2
     ]
+    flags << "-lintl" if OS.mac?
     system ENV.cc, "test.c", "-o", "test", *flags
     system "./test"
   end
