26d5dfb142da3ae4b07373ad749b0038ab1743af
Maxim Belkin <maxim.belkin@gmail.com>
2017-04-25 12:41:50 -0500

    alac: Build a bottle for Linuxbrew

--- Formula/alac.rb
+++ Formula/alac.rb
@@ -1,3 +1,4 @@
+# alac: Build a bottle for Linuxbrew
 class Alac < Formula
   desc "Basic decoder for Apple Lossless Audio Codec files (ALAC)"
   homepage "https://web.archive.org/web/20150319040222/craz.net/programs/itunes/alac.html"
