2018547a4f04aa4e63357e3ecbd0d453c2f5e3be
0x639d1dec74 <JuPlutonic@users.noreply.github.com>
2017-05-27 14:47:55 +0200

    Update mpg123.rb
    Use CoreAudio if it's only MacOSX
    https://github.com/Linuxbrew/homebrew-core/issues/2766

--- Formula/mpg123.rb
+++ Formula/mpg123.rb
@@ -17,10 +17,10 @@ class Mpg123 < Formula
       --disable-debug
       --disable-dependency-tracking
       --prefix=#{prefix}
-      --with-default-audio=coreaudio
       --with-module-suffix=.so
     ]
-
+    args << "--with-default-audio=coreaudio" if OS.mac?
+    
     if MacOS.prefer_64_bit?
       args << "--with-cpu=x86-64"
     else
