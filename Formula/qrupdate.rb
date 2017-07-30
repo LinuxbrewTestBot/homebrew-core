d52a2524083b0c150ecd738ed64325778793e1bc
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-07-30 07:05:37 +0000

    qrupdate: update 1.1.2 bottle for Linuxbrew.

--- Formula/qrupdate.rb
+++ Formula/qrupdate.rb
@@ -9,6 +9,7 @@ class Qrupdate < Formula
     sha256 "797a5f20ea8a61b1c71588b5ccce081daf479239df9e8ac587f52134e3ee2685" => :sierra
     sha256 "5376220c35da94d8f1162dc34624a051fc415363046978f6788678b35af13813" => :el_capitan
     sha256 "2b527726bd3303edc9712e3e47b672a266cbb8065b4e41c51951746f2da8c08d" => :yosemite
+    sha256 "276c64c44deab6a182b0ec297c2b5cb2b528cfd627799a0ce863e741b13aa14f" => :x86_64_linux
   end
 
   depends_on :fortran
