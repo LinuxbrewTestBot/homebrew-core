dbca83fbd3e677a16f7613712f35cb0fa20024ae
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-09 18:41:37 +0000

    armadillo: update 7.960.0 bottle for Linuxbrew.

--- Formula/armadillo.rb
+++ Formula/armadillo.rb
@@ -5,10 +5,11 @@ class Armadillo < Formula
   sha256 "8ea919c4312ec43d454f61fdd0e34282f94739eb51adc78d21176717143cfd32"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
     sha256 "ea5934206eaad18779f7d0aeb11d25db487f0e4254646aa0445212c6fe4e4799" => :sierra
     sha256 "0399afccee447fc70bd3b9a4efd88edb689927baa9323e7a2196f7488313b7c9" => :el_capitan
     sha256 "7f68b75cfd32dd7da7b1d622af91b6ebd9fc190db5e15afe9e23ee8aa7f922dd" => :yosemite
+    sha256 "f3c438fd002a46fc7a7698b5d112785dd66522ff373e1a00960a85187801b325" => :x86_64_linux
   end
 
   option :cxx11
