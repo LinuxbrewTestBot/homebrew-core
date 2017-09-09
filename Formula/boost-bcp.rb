e9fb5ac05cc20d125a8859fe29adc21189a700e3
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-09 20:44:24 +0000

    boost-bcp: update 1.65.1 bottle for Linuxbrew.

--- Formula/boost-bcp.rb
+++ Formula/boost-bcp.rb
@@ -10,6 +10,7 @@ class BoostBcp < Formula
     sha256 "2f9715e15a784c42c02d3e96bbebe5cab5bff01191e63db1649bb4cd529e7fdc" => :sierra
     sha256 "0479bb9b6002bf3c8f0e4063392f67850f34602826d2b3ffccf580509d48d37f" => :el_capitan
     sha256 "eb23497221bd792d1474b2c86e270a492266ea416e33c7bcfe678b4af0581eea" => :yosemite
+    sha256 "951d1fcad695c26be7b44ab227b6ad4839be0f6c6a0162897970f79ffbeee6fb" => :x86_64_linux
   end
 
   depends_on "boost-build" => :build
