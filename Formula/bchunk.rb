afc7b13ece36e91d1db503b0f851833a2bd743a9
Michka Popoff <michkapopoff@gmail.com>
2017-11-19 12:44:28 +0100

    bchunk: Removed wrongly commited patches for Linuxbrew

--- Formula/bchunk.rb
+++ Formula/bchunk.rb
@@ -1,4 +1,3 @@
-# bchunk: Build a bottle for Linuxbrew
 class Bchunk < Formula
   desc "Convert CD images from .bin/.cue to .iso/.cdr"
   homepage "http://he.fi/bchunk/"
@@ -11,17 +10,6 @@ class Bchunk < Formula
     sha256 "d6183607b5b987345ee3380263819f1d5e12f2f3cc9f6fd55accfbf92c26d5ef" => :high_sierra
     sha256 "95ef5fddc2234902187dde834690fb5957bd99ce11403e3d0f8881a705bb8f27" => :sierra
     sha256 "665af973709071e982939f37ba39c79c6e41f7f18277d65670475ba9d8315f94" => :el_capitan
-  end
-
-  # Last upstream release was in 2004, so probably safe to assume this isn't
-  # going away any time soon.
-  patch do
-    url "https://mirrors.ocf.berkeley.edu/debian/pool/main/b/bchunk/bchunk_1.2.0-12.1.debian.tar.xz"
-    mirror "https://mirrorservice.org/sites/ftp.debian.org/debian/pool/main/b/bchunk/bchunk_1.2.0-12.1.debian.tar.xz"
-    sha256 "8c7b530e37f0ebcce673c74962214da02aff7bb1ecc96a4dd359e6115f5c0f57"
-    apply "patches/01-track-size.patch",
-          "patches/CVE-2017-15953.patch",
-          "patches/CVE-2017-15955.patch"
   end
 
   def install
