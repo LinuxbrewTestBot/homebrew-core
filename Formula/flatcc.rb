05589e6c9ff9f400a3181dba49f09d0765ebf8d3
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-20 19:37:27 +0000

    flatcc: update 0.5.1 bottle for Linuxbrew.

--- Formula/flatcc.rb
+++ Formula/flatcc.rb
@@ -11,6 +11,7 @@ class Flatcc < Formula
     sha256 "17b33c3326512047102e7055fa8d7d8b8b6781dde0ed65dd2c38525283f1ce5c" => :high_sierra
     sha256 "972fd44d9828746a29c4eef6f64cfa5fa67508601384117b979d39837e2498d7" => :sierra
     sha256 "ea29799a542aa866c8fe1e5f9416e7bd274edf74da7f400c1d63690c1acd5930" => :el_capitan
+    sha256 "1436a92bc978a2562f123d7dbc618f28d0dd1f6365e0d6d4ce73861abd32649b" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
