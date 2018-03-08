9acb61e84081338026051061896eada9f3c9372f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-08 11:47:58 +0000

    pgcli: update 1.9.0 bottle for Linuxbrew.

--- Formula/pgcli.rb
+++ Formula/pgcli.rb
@@ -9,6 +9,7 @@ class Pgcli < Formula
     sha256 "d9f99b8b2c673e453eb596937f9e7f4494ec4e4fc79c47e1fb0e68bea1162349" => :high_sierra
     sha256 "ff71a9379318190fcde16e718b0c415456557f6d34c0f8dc6d976bfb6a489ccd" => :sierra
     sha256 "5671e0443ff970e652ce47ae1a7ad2a1143828250f71cc3f9a578a8c52c73ffc" => :el_capitan
+    sha256 "bdeb3daf8c9b2f7853937009d65f12b9e0b6d1017bae5dbe5a877c6cfbf9a3ec" => :x86_64_linux
   end
 
   depends_on "python@2" if MacOS.version <= :snow_leopard || !OS.mac?
