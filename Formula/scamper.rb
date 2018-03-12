0a57f9a01d27e4836ff0adeda207b6df99adde42
Michka Popoff <michkapopoff@gmail.com>
2018-03-12 22:07:35 +0100

    scamper: Build a bottle for Linuxbrew

--- Formula/scamper.rb
+++ Formula/scamper.rb
@@ -1,3 +1,4 @@
+# scamper: Build a bottle for Linuxbrew
 class Scamper < Formula
   desc "Advanced traceroute and network measurement utility"
   homepage "https://www.caida.org/tools/measurement/scamper/"
