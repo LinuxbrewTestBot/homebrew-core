40f6faf3434870c8ff1ee96db55d7f075f3d0b33
Michka Popoff <michkapopoff@gmail.com>
2018-03-24 10:53:26 +0100

    frugal: Build a bottle for Linuxbrew

--- Formula/frugal.rb
+++ Formula/frugal.rb
@@ -1,3 +1,4 @@
+# frugal: Build a bottle for Linuxbrew
 class Frugal < Formula
   desc "Cross language code generator for creating scalable microservices"
   homepage "https://github.com/Workiva/frugal"
