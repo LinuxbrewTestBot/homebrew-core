efa47b8fb18a17408a3ca98e685dc64920cde4d2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-19 14:39:29 +0000

    zopfli: update 1.0.2 bottle for Linuxbrew.

--- Formula/zopfli.rb
+++ Formula/zopfli.rb
@@ -10,6 +10,7 @@ class Zopfli < Formula
     sha256 "7e7fdea7ca5ebb6f9c4208e0d75c015d6d0ae3c715f44c3c1f326c1edb3593ff" => :high_sierra
     sha256 "a1256c4c3d2fc456ef2dff647c24dfeadf89fc99775eec17811974d633146ce9" => :sierra
     sha256 "13d19096f3489aa512003b175e3568be65b0a855ddd4cffa624e6a76290f3715" => :el_capitan
+    sha256 "db1e9b8ffb2c091be05de1d7be582ee221abda05247194c88ec5e9c26c048728" => :x86_64_linux
   end
 
   def install
