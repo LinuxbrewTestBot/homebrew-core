bb11f9bcaaf252ca5214d2d9a935f6ec507d03be
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-06-06 13:58:39 +0000

    rust: update 1.26.2 bottle for Linuxbrew.

--- Formula/rust.rb
+++ Formula/rust.rb
@@ -23,6 +23,7 @@ class Rust < Formula
     sha256 "7fc1822dfcc4f1d91c42cc784561e2c11322edd4d502a0a448460f823f9f2154" => :high_sierra
     sha256 "f91e3445af8656a9e257314e493ab52f8b46a0d5eb887098cc40128907e34581" => :sierra
     sha256 "e059c7a87460b42901d63eebb3293f7730719a3174fffc6ec3ebc16360fd1fc9" => :el_capitan
+    sha256 "dd099a3d1f9f32a3ae0441cdd52379cb6ad3eb98352e765077f53a6e95cea152" => :x86_64_linux
   end
 
   head do
