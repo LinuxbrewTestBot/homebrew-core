5c58ca12d2fbee6a291267603634ad0751e08565
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-07-27 05:24:32 +0000

    kubecfg: update 0.9.0 bottle for Linuxbrew.

--- Formula/kubecfg.rb
+++ Formula/kubecfg.rb
@@ -9,6 +9,7 @@ class Kubecfg < Formula
     sha256 "c70bb19117bf8fc90b8d6ca73beb6606dc2be4078c48168d574358298e70f43a" => :high_sierra
     sha256 "04fc44f3869f25d396791233ffae3ae50a172adc4fc2ef4b70fe70931f54f454" => :sierra
     sha256 "b1e86a9bd8512770cf754280962870643d370f06e7eb962babbe15290bacb6e3" => :el_capitan
+    sha256 "584487e2250b31e3de2cb5279a8a59d4cf650a53acc6e09b3ecc141108d015f0" => :x86_64_linux
   end
 
   depends_on "go" => :build
