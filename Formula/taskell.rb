97542d151a0e0b384fc2243badeceec54167f48d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-08-06 21:19:21 +0000

    taskell: update 1.3.2 bottle for Linuxbrew.

--- Formula/taskell.rb
+++ Formula/taskell.rb
@@ -13,6 +13,7 @@ class Taskell < Formula
     sha256 "cee86ae14a02377bbdad3561883d7b3dbe8542cc624a0675663a66e4b32508ea" => :high_sierra
     sha256 "ecbf13ef7e552db7018eeffd94df291c27f1d5175264d400567a430d3b564692" => :sierra
     sha256 "a7d7886bc48bb6a6c9ba8152b08e89e323c002be98b55eb03d6357b7db0500cb" => :el_capitan
+    sha256 "4ac0ee91a08a14529e487c2a055ad35b76599a8d05e2f5977a8e706c2c67cf9a" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
