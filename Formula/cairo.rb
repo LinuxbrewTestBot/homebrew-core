db5ca199baa68ba19b6338524f7b1e0ac479a8d1
Maxim Belkin <maxim.belkin@gmail.com>
2018-09-18 04:59:01 +0000

    cairo: simplifications for Linuxbrew

--- Formula/cairo.rb
+++ Formula/cairo.rb
@@ -4,14 +4,13 @@ class Cairo < Formula
   url "https://cairographics.org/releases/cairo-1.14.12.tar.xz"
   mirror "https://www.mirrorservice.org/sites/ftp.netbsd.org/pub/pkgsrc/distfiles/cairo-1.14.12.tar.xz"
   sha256 "8c90f00c500b2299c0a323dd9beead2a00353752b2092ead558139bd67f7bf16"
-  revision 3 unless OS.mac?
+  revision 4 unless OS.mac?
 
   bottle do
     sha256 "5a6cc135f8a373376dac7d8e2750d10c955fd83977f5549976ad590958971f93" => :mojave
     sha256 "5bdc28de8e5a615ab664d43f7f322ed02d58071171415bb6e2750f486b9465e2" => :high_sierra
     sha256 "102847d74a0a11bb6143d93b9f32e1736e88036fb4c685d554a8bcd376bbd929" => :sierra
     sha256 "bec85433a35605164bdbf5f8913e29eb6d9ceb5acc5569dd9d864706ae6c8d49" => :el_capitan
-    sha256 "f280c34cad69a036fb1f209249e054e78da643481afd8a3962bed5ad91ca9ccb" => :x86_64_linux
   end
 
   devel do
@@ -34,33 +33,25 @@ class Cairo < Formula
   depends_on "pixman"
   unless OS.mac?
     depends_on "zlib"
-    depends_on "linuxbrew/xorg/xorg" => :recommended
-    if build.with?("xorg")
-      depends_on "linuxbrew/xorg/kbproto"
-      depends_on "linuxbrew/xorg/libxcb"
-      depends_on "linuxbrew/xorg/renderproto"
-      depends_on "linuxbrew/xorg/xcb-proto"
-      depends_on "linuxbrew/xorg/xextproto"
-    end
+    depends_on "valgrind" => :recommended
+    depends_on "linuxbrew/xorg/libxext"
+    depends_on "linuxbrew/xorg/libxrender"
   end
 
   def install
+    notmac = OS.mac? ? "no" : "yes"
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
       --enable-gobject=yes
       --enable-svg=yes
       --enable-tee=yes
+      --enable-quartz-image=#{OS.mac? ? "yes" : "no"}
+      --enable-xcb=#{notmac}
+      --enable-xlib=#{notmac}
+      --enable-xlib-xrender=#{notmac}
+      --enable-valgrind=#{build.with?("valgrind") ? "yes" : "no"}
     ]
-    args += %w[
-      --enable-quartz-image
-    ] if OS.mac?
-
-    if !OS.mac? || build.with?("xorg")
-      args << "--enable-xcb=yes" << "--enable-xlib=yes" << "--enable-xlib-xrender=yes"
-    else
-      args << "--enable-xcb=no" << "--enable-xlib=no" << "--enable-xlib-xrender=no"
-    end
 
     if build.head?
       ENV["NOCONFIGURE"] = "1"
