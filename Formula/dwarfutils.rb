9b8a5532b0788ce35fc1cb9cff2f7c4987d9ef8c
Your Name <michkapopoff@gmail.com>
2018-11-04 17:15:01 +0100

    dwarfutils: Fix linkage for Linuxbrew

--- Formula/dwarfutils.rb
+++ Formula/dwarfutils.rb
@@ -12,7 +12,11 @@ class Dwarfutils < Formula
     sha256 "f2909fb054f392f80318f7d621124c6bd5aee4e8637ad2d26f350d8b645ec5ef" => :sierra
   end
 
-  depends_on "libelf" => :build
+  if OS.mac?
+    depends_on "libelf" => :build
+  else
+    depends_on "libelf"
+  end
 
   def install
     system "./configure"
