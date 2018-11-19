6304559cad57b93c6f76fe12ac808bee57511bca
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-19 21:19:38 +0000

    jdupes: update 1.11.1 bottle for Linuxbrew.

--- Formula/jdupes.rb
+++ Formula/jdupes.rb
@@ -9,6 +9,7 @@ class Jdupes < Formula
     sha256 "2adf7e4d8aec28f97bc668c5507e96fb0fb33efc4cfde479e8a8e34ba308d1e9" => :mojave
     sha256 "d16a5dbc7f93cd50f14a1da7572ec6283cf098c12a4331c25070f03284f22251" => :high_sierra
     sha256 "4632167d7896018dad5a016a3f7e6a420fc4d7bc02a36e3e727477272cfe0c3c" => :sierra
+    sha256 "6e3736a29e0461cd7e71f490bf55df33a20469831359b45ec2c3b3cec28e39dd" => :x86_64_linux
   end
 
   def install
