80407753a179d49c85c225bd28789495a2d06a42
Michka Popoff <michkapopoff@gmail.com>
2018-12-02 21:28:21 +0100

    console_bridge: Build a bottle for Linuxbrew

--- Formula/console_bridge.rb
+++ Formula/console_bridge.rb
@@ -1,3 +1,4 @@
+# console_bridge: Build a bottle for Linuxbrew
 class ConsoleBridge < Formula
   desc "Robot Operating System-independent package for logging"
   homepage "https://wiki.ros.org/console_bridge/"
