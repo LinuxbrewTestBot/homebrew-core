b107b972aea95a0a0d756c64ffaca48a007ef5c3
Michka Popoff <michkapopoff@gmail.com>
2018-12-24 16:26:15 +0100

    patchelf: patch for section .tbss not found

--- Formula/patchelf.rb
+++ Formula/patchelf.rb
@@ -3,7 +3,7 @@ class Patchelf < Formula
   homepage "https://nixos.org/patchelf.html"
   url "https://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.gz"
   sha256 "f2aa40a6148cb3b0ca807a1bf836b081793e55ec9e5540a5356d800132be7e0a"
-  revision 1 unless OS.mac?
+  revision 2 unless OS.mac?
 
   bottle do
     cellar :any_skip_relocation
@@ -11,7 +11,6 @@ class Patchelf < Formula
     sha256 "c97defd1aa773a8b3a36ce9941f860bd0d4348c47beb97e2c59e43c096234fd6" => :high_sierra
     sha256 "8994925b02e7d2ce043df104ceb64b959543e1a869dd126b02c00f0f6a20bfd0" => :sierra
     sha256 "38c85e00a1dc54d103713d04a0d458c55cf3f2618d2a7848f5750f66068206d2" => :el_capitan
-    sha256 "00c02e6c700f4427da40a82e2a5d88427cfb28a79b3ac69f441d69d8377254f4" => :x86_64_linux # glibc 2.5
   end
 
   head do
@@ -28,12 +27,13 @@ class Patchelf < Formula
     sha256 "d8c1e93f13e0b7d8fc13ce75d5b089f4d4cec15dad91d08d94a166822d749459"
   end
 
+  # https://github.com/NixOS/patchelf/pull/149
+  patch do
+    url "https://gist.githubusercontent.com/iMichka/5ccb2bac3ee421bc3cbc51079e0e35c8/raw/e848359f5b459b58479d226d349f75337b749f6b/patchelf.patch?full_index=1"
+    sha256 "8d1816eea7407e8fa0b112f84a2e9155f20a7855a8a72c0d88619785d5042390"
+  end
+
   def install
-    # Fixes error: cannot find section
-    # See https://github.com/NixOS/patchelf/pull/95
-    inreplace "src/patchelf.cc",
-      "string sectionName = getSectionName(shdr);",
-      'string sectionName = getSectionName(shdr); if (sectionName == "") continue;'
     system "./bootstrap.sh" if build.head?
     system "./configure", "--prefix=#{prefix}",
       if build.with?("static") then "CXXFLAGS=-static"
