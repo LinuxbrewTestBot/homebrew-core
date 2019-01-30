e462d1b0d037db19df2c8c437a724e4a33ff02ab
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-30 06:51:44 +0000

    krakend: update 0.7.1 bottle for Linuxbrew.

--- Formula/krakend.rb
+++ Formula/krakend.rb
@@ -9,6 +9,7 @@ class Krakend < Formula
     sha256 "857a2b98e19b481b1173772192f3ff01ff1e1ec0636fb57cd362671a5ade9692" => :mojave
     sha256 "7c5b360c7d5f5fd5eadf0975adffaa28787a996c4df937f8f8b5a0648213fc2f" => :high_sierra
     sha256 "e6a11cbb4f03475aee024088a9e6a2d0d128a6116ea81f261ae6342e0cbda097" => :sierra
+    sha256 "47b078803ea3f11794f69bdcc7b27d603932eac5688f064c159cbf502e668344" => :x86_64_linux
   end
 
   depends_on "dep" => :build
