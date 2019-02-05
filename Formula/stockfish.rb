8088155685dbffa54a03d4a63d80c4ad4ae45b01
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-02-05 21:51:11 +0000

    stockfish: update 10 bottle for Linuxbrew.

--- Formula/stockfish.rb
+++ Formula/stockfish.rb
@@ -10,6 +10,7 @@ class Stockfish < Formula
     sha256 "7e7d58466b8d4f325e69eb593dbb40074541e383b9b1e62dae83b3d9cddfc3a7" => :mojave
     sha256 "be82b92aa3b8a89162caca9f206645cb6395b93898f7575ea782f754b2183bd8" => :high_sierra
     sha256 "84e6d5d13b0a30843ed3eafd245a3c6a61ecf67635b3b18ba5950fb69aed1bb1" => :sierra
+    sha256 "1f0963bfc4df4bde85c43c34c6eec458094c1575a6d9f8a26951b11d50c9a452" => :x86_64_linux
   end
 
   def install
