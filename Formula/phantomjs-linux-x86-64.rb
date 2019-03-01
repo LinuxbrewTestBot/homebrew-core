fb62314c26619af6fc3377592c5bc10a89f1d526
Example Example <example@example.org>
2019-03-01 08:59:12 +0530

    adds phantomjs formulae for linux x86-64 based Operating Systems

--- Formula/phantomjs-linux-x86-64.rb
+++ Formula/phantomjs-linux-x86-64.rb
@@ -1,7 +1,7 @@
 class PhantomjsLinuxX8664 < Formula
   desc "This formula is only supported for linux-x86-64 based Operaating systems"
   homepage "http://phantomjs.org/"
-  url "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-#{version}-linux-x86_64.tar.bz2"
+  url "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2"
   sha256 "86dd9a4bf4aee45f1a84c9f61cf1947c1d6dce9b9e8d2a907105da7852460d2f"
   version "2.1.1"
 
