ee03341037bb38d34a1469c677be71d8144a7891
Shaun Jackman <sjackman@gmail.com>
2019-04-04 09:36:04 -0700

    Update Formula/miller.rb
    Co-Authored-By: lcoombe <lauren.e.coombe@gmail.com>

--- Formula/miller.rb
+++ Formula/miller.rb
@@ -26,7 +26,7 @@ class Miller < Formula
     system "./configure", "--prefix=#{prefix}", "--disable-silent-rules",
                           "--disable-dependency-tracking"
     system "make"
-    # Time zone related tests fail
+    # Time zone related tests fail. Reported upstream https://github.com/johnkerl/miller/issues/237
     system "make", "check" unless OS.linux? && ENV["CI"]
     system "make", "install"
   end
