9a2f7c3ec0a431214b383da1dcdf202f538d000d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-10 10:01:10 +0000

    roswell: update 19.4.10.98 bottle.

--- Formula/roswell.rb
+++ Formula/roswell.rb
@@ -6,9 +6,11 @@ class Roswell < Formula
   head "https://github.com/roswell/roswell.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     sha256 "29df3dec523805cf59a014ed724a03b98138484588a16d0ffc3188386e310a98" => :mojave
     sha256 "df711291567a25e3a1783bfe9e05e44e8ba7aaf522b0aae6ba1314c974ec06ac" => :high_sierra
     sha256 "eecd79e1aca9544139fea027739229787b4976f70b14f46f78df310c0ba68ada" => :sierra
+    sha256 "cb5a87c4289de79a6283e0ae9240bd7d4b2f71dfa67df7c8de8acdbad3db7890" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
