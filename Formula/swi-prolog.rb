f29f367f26b7fe25470f18a733b6356c41e1c84b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-10 18:48:56 +0000

    swi-prolog: update 8.0.2_1 bottle.

--- Formula/swi-prolog.rb
+++ Formula/swi-prolog.rb
@@ -11,6 +11,7 @@ class SwiProlog < Formula
     sha256 "1cec5efa06e469a67a7701fa8bc08f320f95ec766f494c305453301a5f8335e6" => :mojave
     sha256 "824e9a80488a9f91f2bab57653baa6794be59c049bdbab464bd8d00510b1147f" => :high_sierra
     sha256 "29f73701075df1cd1bedd01b13fe085d23c74c87285ab12116e1f68554bcfe1b" => :sierra
+    sha256 "74e9fcdc5272cf8c0375138adfc9d1c30b7fa07b121b2b491b27f054a42dfef0" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
