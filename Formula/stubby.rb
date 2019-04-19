13d2291c64d02a73b911fdf5f5dbec02e1905fe2
Michka Popoff <michkapopoff@gmail.com>
2019-04-19 20:32:46 +0200

    stubby: Build a bottle for Linuxbrew

--- Formula/stubby.rb
+++ Formula/stubby.rb
@@ -1,3 +1,4 @@
+# stubby: Build a bottle for Linuxbrew
 class Stubby < Formula
   desc "DNS privacy enabled stub resolver service based on getdns"
   homepage "https://dnsprivacy.org/wiki/display/DP/DNS+Privacy+Daemon+-+Stubby"
