6d3d11bcc30ce2c744bf57d208ae7db488745912
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-04-24 12:24:32 +0900

    Refactor formulae

--- Formula/adoptopenjdk@11.rb
+++ Formula/adoptopenjdk@11.rb
@@ -1,33 +1,33 @@
-    class AdoptopenjdkAT11 < Formula
-      desc "AdoptOpenJDK are prebuilt binaries of OpenJDK"
-      homepage "https://adoptopenjdk.net/"
-      url "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7/OpenJDK11U-jdk_x64_linux_hotspot_11.0.3_7.tar.gz"
-      sha256 "23cded2b43261016f0f246c85c8948d4a9b7f2d44988f75dad69723a7a526094"
-      option "with-openj9", "Install OpenJ9 instead of HotSpot"
-      depends_on :linux
-    
-      def install
-        if build.with? "openj9"
-            system "rm -rf *"
-            system "wget -O- https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7_openj9-0.14.0/OpenJDK11U-jdk_x64_linux_openj9_11.0.3_7_openj9-0.14.0.tar.gz | tar --strip-components=1 -zxf -"
-        end
-        prefix.install Dir["*"]
-        share.mkdir
-      end
-    
-      test do
-        (testpath/"Hello.java").write <<~EOS
-          class Hello
-          {
-            public static void main(String[] args)
-            {
-              System.out.println("Hello Homebrew");
-            }
-          }
-        EOS
-        system bin/"javac", "Hello.java"
-        assert_predicate testpath/"Hello.class", :exist?, "Failed to compile Java program!"
-        assert_equal "Hello Homebrew
-", shell_output("#{bin}/java Hello")
-      end
-    end
+class AdoptopenjdkAT11 < Formula
+  desc "AdoptOpenJDK are prebuilt binaries of OpenJDK"
+  homepage "https://adoptopenjdk.net/"
+  if build.with? "openj9"
+    url "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7_openj9-0.14.0/OpenJDK11U-jdk_x64_linux_openj9_11.0.3_7_openj9-0.14.0.tar.gz"
+    sha256 "8ed83ebed830539c93545b8095d08a5bd48946ab1bdc523e39320417a4d59909"
+  else
+    url "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7/OpenJDK11U-jdk_x64_linux_hotspot_11.0.3_7.tar.gz"
+    sha256 "39dad3e7d94fd8d313d24563677316cb72c0490af50de4f852bc5217e58c098e"
+  end
+  option "with-openj9", "Install OpenJ9 instead of HotSpot"
+  depends_on :linux
+
+  def install
+    prefix.install Dir["*"]
+    share.mkdir
+  end
+
+  test do
+    (testpath/"Hello.java").write <<~EOS
+      class Hello
+      {
+        public static void main(String[] args)
+        {
+          System.out.println("Hello Homebrew");
+        }
+      }
+    EOS
+    system bin/"javac", "Hello.java"
+    assert_predicate testpath/"Hello.class", :exist?, "Failed to compile Java program!"
+    assert_equal "Hello Homebrew\n", shell_output("#{bin}/java Hello")
+  end
+end
--- Formula/adoptopenjdk@8.rb
+++ Formula/adoptopenjdk@8.rb
@@ -1,33 +1,33 @@
-    class AdoptopenjdkAT8 < Formula
-      desc "AdoptOpenJDK are prebuilt binaries of OpenJDK"
-      homepage "https://adoptopenjdk.net/"
-      url "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03/OpenJDK8U-jdk_x64_linux_hotspot_8u212b03.tar.gz"
-      sha256 "dd28d6d2cde2b931caf94ac2422a2ad082ea62f0beee3bf7057317c53093de93"
-      option "with-openj9", "Install OpenJ9 instead of HotSpot"
-      depends_on :linux
-    
-      def install
-        if build.with? "openj9"
-            system "rm -rf *"
-            system "wget -O- https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03_openj9-0.14.0/OpenJDK8U-jdk_x64_linux_openj9_8u212b03_openj9-0.14.0.tar.gz | tar --strip-components=1 -zxf -"
-        end
-        prefix.install Dir["*"]
-        share.mkdir
-      end
-    
-      test do
-        (testpath/"Hello.java").write <<~EOS
-          class Hello
-          {
-            public static void main(String[] args)
-            {
-              System.out.println("Hello Homebrew");
-            }
-          }
-        EOS
-        system bin/"javac", "Hello.java"
-        assert_predicate testpath/"Hello.class", :exist?, "Failed to compile Java program!"
-        assert_equal "Hello Homebrew
-", shell_output("#{bin}/java Hello")
-      end
-    end
+class AdoptopenjdkAT8 < Formula
+  desc "AdoptOpenJDK are prebuilt binaries of OpenJDK"
+  homepage "https://adoptopenjdk.net/"
+  if build.with? "openj9"
+    url "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03_openj9-0.14.0/OpenJDK8U-jdk_x64_linux_openj9_8u212b03_openj9-0.14.0.tar.gz"
+    sha256 "56f9bdbb3962a5f149ce2e4946f7bd7d12c3b70a1d7df91bbd46fe55f9553421"
+  else
+    url "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03/OpenJDK8U-jdk_x64_linux_hotspot_8u212b03.tar.gz"
+    sha256 "dd28d6d2cde2b931caf94ac2422a2ad082ea62f0beee3bf7057317c53093de93"
+  end
+  option "with-openj9", "Install OpenJ9 instead of HotSpot"
+  depends_on :linux
+
+  def install
+    prefix.install Dir["*"]
+    share.mkdir
+  end
+
+  test do
+    (testpath/"Hello.java").write <<~EOS
+      class Hello
+      {
+        public static void main(String[] args)
+        {
+          System.out.println("Hello Homebrew");
+        }
+      }
+    EOS
+    system bin/"javac", "Hello.java"
+    assert_predicate testpath/"Hello.class", :exist?, "Failed to compile Java program!"
+    assert_equal "Hello Homebrew\n", shell_output("#{bin}/java Hello")
+  end
+end
