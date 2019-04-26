63609797f8d2e25834bcf00c09531815dad21575
TANIGUCHI Masaya <xtaniguchimasaya+code@gmail.com>
2019-04-26 13:59:23 +0900

    Change version format for jdk8u

--- Formula/adoptopenjdk.rb
+++ Formula/adoptopenjdk.rb
@@ -2,7 +2,7 @@ class Adoptopenjdk < Formula
   desc "Prebuilt binaries produced from OpenJDK class libraries"
   homepage "https://adoptopenjdk.net/"
   url "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03/OpenJDK8U-jdk_x64_linux_hotspot_8u212b03.tar.gz"
-  version "8.0.212"
+  version "1.8.0.212"
   sha256 "dd28d6d2cde2b931caf94ac2422a2ad082ea62f0beee3bf7057317c53093de93"
 
   depends_on :linux
