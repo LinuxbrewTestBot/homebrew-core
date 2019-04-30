5a90957b1d753d5b840dfa1b412ea4fead1acfb7
Lauren Coombe <lauren.e.coombe@gmail.com>
2019-04-30 10:23:04 -0700

    sratoolkit: fix build for linux

--- Formula/sratoolkit.rb
+++ Formula/sratoolkit.rb
@@ -6,7 +6,6 @@ class Sratoolkit < Formula
   head "https://github.com/ncbi/sra-tools.git"
 
   bottle do
-    cellar :any
     sha256 "3f096fe6b0e114fa80664808855571f1f4b5a90791d0ff27344045541fabcc1f" => :mojave
     sha256 "eae0fff7e32f0c681c804686b5dc7dacbd8d051cc2538a3344b43c3e3c7c7b8d" => :high_sierra
     sha256 "0f6ea3b9f3138766a401fa0c892c888eeaeabc95409d26669985ce72aca3f123" => :sierra
