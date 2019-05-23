0af246171761d441931fb7ac1dba25e623108638
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-23 23:42:29 +0000

    go@1.11: update 1.11.6 bottle.

--- Formula/go@1.11.rb
+++ Formula/go@1.11.rb
@@ -9,6 +9,7 @@ class GoAT111 < Formula
     sha256 "ea7bc6953386d6c918bc4cb04b2ae9b9c6630c408690abccf3a183b98121550f" => :mojave
     sha256 "77ed9c049a790f3ab15d38051deca77b68f8666a225e881015fe5f56734ab6ec" => :high_sierra
     sha256 "6010340eb598272a6b256e9e2287c6cfea2d2e20ff97d548d7aa1962237d7d56" => :sierra
+    sha256 "612786739047894786a727ee80c8342c515f4ed820986b9c82cb7bf697c974af" => :x86_64_linux
   end
 
   keg_only :versioned_formula
