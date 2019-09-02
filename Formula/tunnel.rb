6bc7220c7d6497af95008f87052937f147adeb5d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-02 16:15:31 +0000

    tunnel: update 0.4.2 bottle.

--- Formula/tunnel.rb
+++ Formula/tunnel.rb
@@ -9,6 +9,7 @@ class Tunnel < Formula
     sha256 "cb59b08714760f8c0700df546de4211f881769fadafd31fc1df3ba173b9ecddf" => :mojave
     sha256 "60f4fe537955f4290bfad66fe86280291adbd930663317e8ec95af5703eb715b" => :high_sierra
     sha256 "542b9b72465f423656d687534af0b25c468f62c97d0531e34d15abb533cd51d9" => :sierra
+    sha256 "932a375f76e0647e90260aea9a93b585feed5f708d73b71c01db093ddff5e52c" => :x86_64_linux
   end
 
   depends_on "go" => :build
