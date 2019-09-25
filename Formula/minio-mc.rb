068c23971f097170c95c7c5598a9b7f53cab1cff
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-25 17:53:01 +0000

    minio-mc: update 20190924013620 bottle.

--- Formula/minio-mc.rb
+++ Formula/minio-mc.rb
@@ -12,6 +12,7 @@ class MinioMc < Formula
     sha256 "1db416c0d28b3b1cb48d31d52907c6055e1342e5fbf324f258dedf92447ffb66" => :mojave
     sha256 "a53909264b73fab0875bc13f53f8ea7f57bde2e536f0d538c6c0f95cea3c7e45" => :high_sierra
     sha256 "d3dfaf021f9abfc0bc63e5adf358ec0f3794873f5298f26d7f304dadaf6ea611" => :sierra
+    sha256 "8d5cc281d693e98d1f5767ae0395dceb2fce76c3bec91d89fee4cd1b913fa626" => :x86_64_linux
   end
 
   depends_on "go" => :build
