0f9fe392c4c88e81bcd7be115ab06c823932f5d3
Feng Yu <abcfy2@163.com>
2019-10-16 15:43:02 +0800

    Make aria2 support most features.

--- Formula/aria2.rb
+++ Formula/aria2.rb
@@ -3,18 +3,19 @@ class Aria2 < Formula
   homepage "https://aria2.github.io/"
   url "https://github.com/aria2/aria2/releases/download/release-1.35.0/aria2-1.35.0.tar.xz"
   sha256 "1e2b7fd08d6af228856e51c07173cfcf987528f1ac97e04c5af4a47642617dfd"
+  revision 1 unless OS.mac?
 
   bottle do
     sha256 "9cc5e04be8b0a58d1f2b60b8abfc636168edbf23e7018003c40f1dd6952aab0c" => :catalina
     sha256 "761836ac608eb0a59d4a6f6065860c0e809ce454692e0937d9d0d89ad47f3ce4" => :mojave
     sha256 "70cc7566a23c283015368f92dfeaa0d119e53cfc7c1b2276a73ff9f6167b529d" => :high_sierra
-    sha256 "12d575bcdd389906c38dee7968a5509a8417fec1d0fc3ef866c09b977e5454d3" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
   depends_on "libssh2"
   unless OS.mac?
-    depends_on "openssl"
+    depends_on "gnutls"
+    depends_on "c-ares"
     depends_on "zlib"
   end
 
@@ -22,16 +23,30 @@ class Aria2 < Formula
     ENV.cxx11
 
     args = %W[
-      --disable-dependency-tracking
       --prefix=#{prefix}
-      #{OS.mac? ? "--with-appletls" : "--without-appletls"}
       --with-libssh2
-      #{OS.mac? ? "--without-openssl" : "--with-openssl"}
-      --without-gnutls
-      --without-libgmp
-      --without-libnettle
+      --without-openssl
       --without-libgcrypt
+      --enable-libaria2
+      --disable-silent-rules
+      --disable-maintainer-mode
+      --disable-dependency-tracking
     ]
+
+    # Some options come from Ubuntu:
+    # https://launchpadlibrarian.net/420364343/buildlog_ubuntu-eoan-amd64.aria2_1.34.0-4_BUILDING.txt.gz
+    if OS.mac?
+      args << "--with-appletls"
+      args << "--without-gnutls"
+      args << "--without-libgmp"
+      args << "--without-libnettle"
+    elsif OS.linux?
+      args << "--without-appletls"
+      args << "--with-gnutls"
+      args << "--with-libgmp"
+      args << "--with-libnettle"
+      args << "--with-ca-bundle=#{etc}/openssl@1.1/cert.pem"
+    end
 
     system "./configure", *args
     system "make", "install"
