ed722e02f48c519cfa1ff447e7e1a216c48e92db
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-30 20:48:36 +0000

    onefetch: update 1.7.0 bottle.

--- Formula/onefetch.rb
+++ Formula/onefetch.rb
@@ -9,6 +9,7 @@ class Onefetch < Formula
     sha256 "24b7e0e08d574596b3ecf58979250cf86929c7441a90595b4df889035409440a" => :catalina
     sha256 "74b26c65cadc88c84016d41ab1b656082a5bab933d9fee970e0e4975ed44306c" => :mojave
     sha256 "d2b3cd46a46949c0e23dedc44bace6f1532c45b1eb241c27610f55395d131060" => :high_sierra
+    sha256 "8aea1ef19fa5ca83c4d5012ff8a521311364b189c5c61f302e8e783cbbc36464" => :x86_64_linux
   end
 
   depends_on "rust" => :build
