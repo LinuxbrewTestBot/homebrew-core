89b17930307106083a2f8b6996bfa64faf89b64c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-30 20:48:36 +0000

    tdkjs: update 3.2.0 bottle.

--- Formula/tdkjs.rb
+++ Formula/tdkjs.rb
@@ -11,6 +11,7 @@ class Tdkjs < Formula
     sha256 "d59d6292d1982f2bfb6fac6f3e25d638b131b2ad9c36598ef5ad5ee4f5eb76e3" => :catalina
     sha256 "9edeabf99f8d427626ee1500981ed3bb3738be523abcfa600457588afca605c7" => :mojave
     sha256 "09ace9b6ab20a91e582b8d95ba05abe45b046231cb457cf87c7d2704352778f8" => :high_sierra
+    sha256 "84ce8e72490acc9ad44a5b4f042183169cf355addd1dd39921b09dc43401a0c7" => :x86_64_linux
   end
 
   depends_on "node"
