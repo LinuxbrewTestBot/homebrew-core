99cb71cae7654ededf8b5726fab6cac6eeee05a6
Michka Popoff <michkapopoff@gmail.com>
2019-11-20 21:58:44 +0100

    diamond: Build a bottle for Linux

--- Formula/diamond.rb
+++ Formula/diamond.rb
@@ -1,3 +1,4 @@
+# diamond: Build a bottle for Linux
 class Diamond < Formula
   desc "Accelerated BLAST compatible local sequence aligner"
   homepage "https://ab.inf.uni-tuebingen.de/software/diamond/"
