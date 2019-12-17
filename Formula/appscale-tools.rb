f93273233138cb5641fb1dde35034856a92a6423
Dawid Dziurla <dawidd0811@gmail.com>
2019-12-17 15:20:41 +0100

    appscale-tools: Build a bottle for Linux

--- Formula/appscale-tools.rb
+++ Formula/appscale-tools.rb
@@ -1,3 +1,4 @@
+# appscale-tools: Build a bottle for Linux
 class AppscaleTools < Formula
   desc "Command-line tools for working with AppScale"
   homepage "https://github.com/AppScale/appscale-tools"
