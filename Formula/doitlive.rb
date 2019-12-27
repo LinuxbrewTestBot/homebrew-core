1360e0a99b5ad04345dcaef89ba0429aac21e599
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-27 22:30:08 +0000

    doitlive: update 4.3.0_5 bottle.

--- Formula/doitlive.rb
+++ Formula/doitlive.rb
@@ -10,6 +10,7 @@ class Doitlive < Formula
     sha256 "2e74813a2bfca519ee93251fab563d249515c322ce2b51ee253754b7fbc8e433" => :catalina
     sha256 "0d150f01d69513f9db559310fb643ba412929ae098e782233a91e2302c1fff1d" => :mojave
     sha256 "29aa9f2ede3c4a20c9b06181e26088f96a05354adf3269acda5e0ec756021c40" => :high_sierra
+    sha256 "1b2fe1c9a734a64ecb5e54bb31a54b689b3d734fa8bf9b20630fcddcb6ce10ba" => :x86_64_linux
   end
 
   depends_on "python"
