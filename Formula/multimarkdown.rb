c7b7a543eb750261cba7eac67559c0c3570b2df7
Dawid Dziurla <dawidd0811@gmail.com>
2020-01-12 13:08:49 +0100

    multimarkdown: Build a bottle for Linux

--- Formula/multimarkdown.rb
+++ Formula/multimarkdown.rb
@@ -1,3 +1,4 @@
+# multimarkdown: Build a bottle for Linux
 class Multimarkdown < Formula
   desc "Turn marked-up plain text into well-formatted documents"
   homepage "https://fletcher.github.io/MultiMarkdown-6/"
